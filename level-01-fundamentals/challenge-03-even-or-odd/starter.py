d96826f3a426068fda32a9ebdbdaae2ec092b69d
harishkumarkrst <harishkumarkrst@gmail.com>
2026-06-18 12:59:42 +0530

    Complete challenge 03: even or odd

--- level-01-fundamentals/challenge-03-even-or-odd/starter.py
+++ level-01-fundamentals/challenge-03-even-or-odd/starter.py
@@ -1,51 +1,14 @@
 def is_even(number):
-    """
-    TODO:
-    Return True if the number is even, False if the number is odd.
-
-    A number is even when dividing it by 2 leaves no remainder.
-
-    Examples:
-        is_even(4)  should return True
-        is_even(7)  should return False
-        is_even(0)  should return True   (zero is even!)
-        is_even(-4) should return True   (negative even numbers count)
-
-    Hint: Use the modulo operator: number % 2
-          If number % 2 == 0, the number is even.
-    """
-    pass
+    return number % 2 == 0
 
 
 def is_odd(number):
-    """
-    TODO:
-    Return True if the number is odd, False if the number is even.
-
-    Examples:
-        is_odd(3)   should return True
-        is_odd(-4)  should return False
-        is_odd(0)   should return False
-        is_odd(7)   should return True
-
-    Hint: A number is odd when it is NOT even.
-          You can use your is_even() function here, or check number % 2 != 0.
-    """
-    pass
-
+    return number % 2 != 0
 
 def classify_number(number):
-    """
-    TODO:
-    Return the string "even" if the number is even,
-    or the string "odd" if the number is odd.
+    if is_even(number):
+        return "even"
+    else:
+        return "odd"
 
-    Examples:
-        classify_number(6)  should return "even"
-        classify_number(9)  should return "odd"
-        classify_number(0)  should return "even"
-        classify_number(-3) should return "odd"
-
-    Note: Return the word as a lowercase string: "even" or "odd"
-    """
-    pass
+classify_number(2)
